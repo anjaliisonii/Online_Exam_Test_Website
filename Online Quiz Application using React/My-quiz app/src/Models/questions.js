53cc5f43e1e572701d781a1a669c6ef568a5d12a
Anjali <95306164+anjaliisonii@users.noreply.github.com>
2023-11-25 18:46:58 +0530

    Add files via upload

---
+++ Online Quiz Application using React/My-quiz app/src/Models/questions.js
@@ -0,0 +1,13 @@
+// models/Question.js
+class Question {
+    constructor(question, subject, topic, difficulty, marks) {
+      this.question = question;
+      this.subject = subject;
+      this.topic = topic;
+      this.difficulty = difficulty;
+      this.marks = marks;
+    }
+  }
+  
+  module.exports = Question;
+  
